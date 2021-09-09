93d8c6496d784cbb26eebcf4e7542e54099c4144
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-09-09 14:16:31 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('calculadora.html')
+
+@app.route('/log')
+def log():
+    return render_template('logs.html')
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
